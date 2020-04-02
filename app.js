2fbdf6705a123c2546c3bde2702b6078cb7e6ce0
Malik-G <Malik-G@users.noreply.github.com>
2020-04-02 01:30:30 -0500

    added some click events and dynamic rendering

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const expense = document.querySelector('#input-expense');
 const amount = document.querySelector('#input-amount');
 const addExpense = document.querySelector('#btn-add');
-const totalSpan = document.querySelector('#total-amount');
 const clearHistory = document.querySelector('#btn-clear');
+const totalSpan = document.querySelector('#total-amount');
 const expenseList = document.querySelector('#expense-list');
 
 let currentTotal = 0
@@ -21,7 +21,7 @@ addExpense.addEventListener('click', () => {
   else {
     newItem.textContent = `- ${expense.value}: $${amount.value}`;
     currentTotal = currentTotal + parseInt(amount.value);
-    totalSpan.innerHTML = currentTotal;
+    totalSpan.textContent = currentTotal;
     expenseList.appendChild(newItem);
     expense.value = '';
     amount.value = '';
@@ -29,5 +29,9 @@ addExpense.addEventListener('click', () => {
 });
 
 clearHistory.addEventListener('click', () => {
-
+  currentTotal = 0
+  totalSpan.innerHTML = '';
+  expenseList.innerHTML = '';
+  expense.value = '';
+  amount.value = '';
 })
